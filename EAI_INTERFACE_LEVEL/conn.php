2fda6b92716bca26ba16b077a19cbe8d54c9a666
Aria Fajar Ramdhany <aria.fajar14@gmail.com>
2021-03-30 13:26:55 +0700

    belum beres

---
+++ EAI_INTERFACE_LEVEL/conn.php
@@ -0,0 +1,10 @@
+<?php
+//konfig server
+$server="localhost";
+$user="root";
+$pass="";
+$database="eai_scrapping";
+
+
+$con = mysqli_connect($server, $user, $pass, $database) or die('Error Connection Network');
+?>
---
+++ EAI_INTERFACE_LEVEL/index.php
@@ -0,0 +1,182 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <title>EAI SCRAPPING </title>
+</head>
+
+<body>
+    <h1 align="center">DAFTAR LAPTOP</h1><br>
+
+    <div class="d-flex align-items-start">
+        <div class="nav flex-column nav-pills me-5" id="v-pills-tab" role="tablist" aria-orientation="vertical" style="position: fixed; margin-top:48px;">
+            <button class="nav-link active" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="#v-pills-home"
+                type="button" role="tab" aria-controls="v-pills-home" aria-selected="true"
+                style="width:100px; height:200px;">ASUS</button>
+            <button class="nav-link" id="v-pills-profile-tab" data-bs-toggle="pill" data-bs-target="#v-pills-profile"
+                type="button" role="tab" aria-controls="v-pills-profile" aria-selected="false"
+                style="width:100px; height:200px;">HP</button>
+        </div>
+        <div class="tab-content" id="v-pills-tabContent" style="margin-left: 140px;">
+
+            <!-- HALAMAN LAPTOP ASUS -->
+            <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
+                <!-- CARD LAPTOP ASUS -->
+                <div class="row row-cols-2 row-cols-md-4 g-4 mr-auto ml-10" style="width: 1200px;">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- HALAMAN LAPTOP HP -->
+            <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+                <!-- CARD LAPTOP HP -->
+                <div class="row row-cols-2 row-cols-md-4 g-4 mr-auto ml-10" style="width: 1200px;">
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://p.ipricegroup.com/uploaded_cbab94b165783fbf63bc824ed8a3fc43.jpg?position=1"
+                                class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nama</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title">Rp. 100.000</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<br>
+<br>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
